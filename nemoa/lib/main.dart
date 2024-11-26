0c7f5332f03153fbeb1850c44fc63e2be1ba08bc
DeysiRR <deysi.rufino@uabc.edu.mx>
2024-11-26 00:13:06 -0800

    Encripta passwords

--- nemoa/lib/main.dart
+++ nemoa/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:nemoa/presentation/screens/login_page.dart';
 import 'package:nemoa/presentation/screens/main_page.dart';
 import 'package:nemoa/presentation/screens/messages_page.dart';
 import 'package:nemoa/presentation/screens/personalization_page.dart';
+import 'package:nemoa/presentation/screens/reset_password_page.dart';
 import 'package:nemoa/presentation/screens/signup_pag.dart';
 import 'package:nemoa/presentation/screens/user_profile_page.dart';
 import 'package:nemoa/presentation/screens/test_page.dart';
@@ -62,6 +63,7 @@ class MyApp extends StatelessWidget {
         UserProfilePage.routename: (context) => const UserProfilePage(),
         TestPage.routename: (context) => TestPage(key: testPageKey),
         ForgotPasswordPage.routename: (context) => const ForgotPasswordPage(),
+        ResetPasswordPage.routename: (context) => const ResetPasswordPage(),
       },
     );
   }
